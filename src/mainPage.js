fc542bb18d3837f1e9ed864619028b9f4037b528
Steven Busenbarrick <sbusenba@gmail.com>
2022-10-02 11:10:55 -0700

    update css and mainpage logo

--- src/mainPage.js
+++ src/mainPage.js
@@ -5,17 +5,19 @@ mainDiv.classList.add('menuDiv')
 let titleDiv = document.createElement('div')
 titleDiv.classList.add('titleDiv')
 titleDiv.innerText = 'Malarky'
-mainDiv.appendChild(titleDiv)
-
 let pic = document.createElement('div')
+pic.classList.add("mainImgHolder")
 let img = document.createElement('img')
 img.classList.add(`mainPic`)
 pic.appendChild(img)
+pic.appendChild(titleDiv)
+let paragraphDiv = document.createElement('div')
+paragraphDiv.classList.add("paragraphDiv")
+paragraphDiv.innerText = 'Fine imaginary foods for the distinguished palate'
+pic.appendChild(paragraphDiv)
 mainDiv.appendChild(pic)
 
-let paragraphDiv = document.createElement('div')
-paragraphDiv.innerText = 'Fine imaginary foods for the distinguished palate'
-mainDiv.appendChild(paragraphDiv)
+
 
 
 
